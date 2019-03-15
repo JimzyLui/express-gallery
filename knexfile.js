9c329941b600c026960ff34a864af610eb84aec9
Jennifer Enomoto-Hahn <hugomcmittens@Jennifers-Air.home>
2019-03-14 21:52:00 -1000

    rewrote package.json file

--- knexfile.js
+++ knexfile.js
@@ -6,7 +6,7 @@ require('dotenv').config();
 module.exports = {
   client: 'pg',
   connection: {
-    port: process.env.POSTGRES_HOST_PORT,
+    // port: process.env.POSTGRES_HOST_PORT,
     database: process.env.POSTGRES_DB,
     user: process.env.POSTGRES_USER,
     password: process.env.POSTGRES_PASSWORD
