56b607963771e648de7539c190bc1d95fa41fc63
Jimzy <JimzyLui@users.noreply.github.com>
2019-03-14 21:13:35 -1000

    fixed db locations

---
+++ knexfile.js
@@ -0,0 +1,26 @@
+// Update with your config settings.
+require('dotenv').config();
+// console.log(process.env);
+// require('dotenv').config({path: '.'});
+
+module.exports = {
+  client: 'pg',
+  connection: {
+    port: process.env.POSTGRES_HOST_PORT,
+    database: process.env.POSTGRES_DB,
+    user: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD
+  },
+  seeds: {
+    directory: './resources/database/seeds'
+  },
+  pool: {
+    min: 2,
+    max: 10
+  },
+  migrations: {
+    directory: './resources/database/migrations',
+    tableName: 'knex_migrations'
+  }
+};
+
