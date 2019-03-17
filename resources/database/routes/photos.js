69dac5d1cb8103ca6b7fac8a91c8763d7a646863
Jennifer Enomoto-Hahn <hugomcmittens@Jennifers-Air.home>
2019-03-16 17:50:47 -1000

    added into the routes a get request for photos/edit

--- resources/database/routes/photos.js
+++ resources/database/routes/photos.js
@@ -1,13 +1,13 @@
-'use strict';
-var express = require('express');
-var Photo = require('../models/photo');
+"use strict";
+var express = require("express");
+var Photo = require("../models/photo");
 var router = express.Router();
-router.route('/')
-  .get(function(req, res) {
-    User
-      .fetchAll()
-      .then(function(photos) {
-        res.json({ photos });
-      });
+router.route("/").get(function(req, res) {
+  User.fetchAll().then(function(photos) {
+    res.json({ photos });
   });
+});
+router.route("/edit").get(function(req, res) {
+  res.render("edit");
+});
 module.exports = router;
